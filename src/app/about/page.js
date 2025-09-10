16f8be702b434f91f741d326af2d0386d3d3a875
shady <shady@billmyplate.com>
2025-09-10 15:31:56 +0300

    fixed layout

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,3 @@
-import { Header } from "../components/header";
-import { Footer } from "../components/footer";
 import WhoWeAreSection from "../sections/WhoWeAreSection";
 import MeetTheTeam from "../sections/MeetTheTeam";
 import OurProcess from "../sections/OurProcess";
@@ -7,12 +5,10 @@ import VisionMissionSection from "../sections/VisionMissionSection";
 export default function AboutPage() {
   return (
     <>
-      <Header />
       <WhoWeAreSection />
       <MeetTheTeam />
       <OurProcess />
       <VisionMissionSection />
-      <Footer />
     </>
   );
 }
